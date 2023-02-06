7e6c3617b320276b3be6a24e75a82cb163b51f5a
JosephEJohnston <1193347519@qq.com>
2023-02-06 17:56:53 +0800

    32. 授权模式

--- demo/src/main/java/com/noob/demo/package-info.java
+++ demo/src/main/java/com/noob/demo/package-info.java
@@ -19,4 +19,17 @@ package com.noob.demo;
  * Oauth2 认证：
  * Oauth2 提供了用户认证（开放资源授权）的标准
  * 见图：static/Oauth2.png
+ *
+ * 授权模式：
+ *  1. 授权码模式，最复杂，最安全，用得最多
+ *      见图：static/授权码模式.png
+ *  2. 简化授权模式
+ *      见图：static/简化授权模式.png
+ *  3. 密码模式
+ *      见图：static/密码模式.png
+ *  4. 客户端模式
+ *      见图：static/客户端模式.png
+ *
+ * 刷新令牌：
+ *  见图：static/刷新令牌.png
  */
